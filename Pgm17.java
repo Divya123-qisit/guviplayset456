6aeeceed5c51ae72fb2fa20c79c3cb7f34952b24
udhayapriyaalamelu <40052766+udhayapriyaalamelu@users.noreply.github.com>
2018-08-09 15:08:45 +0530

    Add files via upload

---
+++ Pgm17.java
@@ -0,0 +1,17 @@
+package guviplayset456;
+import java.util.*;
+public class Pgm17 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+int b=sc.nextInt();
+int c=sc.nextInt();
+if(a==b||b==c||a==c)
+	System.out.println("yes");
+else
+	System.out.println("no");
+	}
+
+}
