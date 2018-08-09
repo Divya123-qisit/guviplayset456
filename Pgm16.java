756ad9e6e46a117fb84b28557498f932023addca
udhayapriyaalamelu <40052766+udhayapriyaalamelu@users.noreply.github.com>
2018-08-09 14:47:32 +0530

    Add files via upload

---
+++ Pgm16.java
@@ -0,0 +1,14 @@
+package guviplayset456;
+import java.util.*;
+public class Pgm16 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+float s;
+s=(float) (a*(3.14/180));
+System.out.println(s);
+	}
+
+}
