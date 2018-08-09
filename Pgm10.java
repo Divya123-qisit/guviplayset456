893316ddf69f07cddc65ad52ba87c3650c9823df
udhayapriyaalamelu <40052766+udhayapriyaalamelu@users.noreply.github.com>
2018-08-09 13:33:55 +0530

    Add files via upload

---
+++ Pgm10.java
@@ -0,0 +1,14 @@
+package guviplayset456;
+import java.util.*;
+public class Pgm10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int count=0;
+count=n/2+1;
+System.out.println(count);
+	}
+
+}
