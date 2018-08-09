ec99c21cfa99ced8f3b4f68430ac83c12ae06885
udhayapriyaalamelu <40052766+udhayapriyaalamelu@users.noreply.github.com>
2018-08-09 15:19:18 +0530

    Add files via upload

---
+++ Pgm19.java
@@ -0,0 +1,15 @@
+package guviplayset456;
+import java.util.*;
+public class Pgm19 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+if((n>=-2147483648)||(n<=2147483647))
+	System.out.println("Int");
+else
+	System.out.println("invalid");
+	}
+
+}
